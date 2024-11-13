96732d08947e00a97cf1b4ed59c9e016280824c3
Alessandra <alessandradirosa@gmail.com>
2024-11-13 12:44:29 +0100

    Initial Commit

---
+++ ImpostorSyndromeAl/ImpostorSyndromeAlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpostorSyndromeAlApp.swift
+//  ImpostorSyndromeAl
+//
+//  Created by Alessandra Di Rosa on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ImpostorSyndromeAlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImpostorSyndromeAlTests/ImpostorSyndromeAlTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImpostorSyndromeAlTests.swift
+//  ImpostorSyndromeAlTests
+//
+//  Created by Alessandra Di Rosa on 13/11/24.
+//
+
+import Testing
+@testable import ImpostorSyndromeAl
+
+struct ImpostorSyndromeAlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
