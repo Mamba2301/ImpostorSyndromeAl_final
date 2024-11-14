d67332570120aa866e29543ad04620edf7226edb
Alessandra <alessandradirosa@gmail.com>
2024-11-14 10:49:07 +0100

    giovanni hai rotto

---
+++ ImpostorSyndromeAl/MainView.swift
@@ -0,0 +1,8 @@
+//
+//  MainView.swift
+//  ImpostorSyndromeAl
+//
+//  Created by Alessandra Di Rosa on 13/11/24.
+//
+
+import Foundation
---
+++ ImpostorSyndromeAl/PopOver.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ImpostorSyndromeAl
+//
+//  Created by Alessandra Di Rosa on 13/11/24.
+//
+
+import Foundation
